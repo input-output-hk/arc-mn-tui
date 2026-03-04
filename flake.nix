ce8129cf9f5683ef239630273d98567a451bc689
Brian W Bush <brian.bush@iohk.io>
2026-03-04 10:37:17 -0700

    Added vscode support to nix flake

--- flake.nix
+++ flake.nix
@@ -9,13 +9,19 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs_20
             bubblewrap # For unprivileged sandboxing
+            vscode
           ];
 
           shellHook = ''
